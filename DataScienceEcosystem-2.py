f0c7db5c5195f0d126a530ec3c5d3de004bec12a
HemanthNarlaSubramanyam <160323644+HemanthNarlaSubramanyam@users.noreply.github.com>
2024-07-03 23:46:37 +0200

    Assignment

---
+++ DataScienceEcosystem-2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# ### Objectives:
+
+# #### The 3 commonly used data science languages:
+
+#  - Python
+#  - R
+#  - SQL
+
+# **Some of the commonly used libraries used by Data Scientists include:** NumPy, Pandas,and Matplotlib
+# 
+
+# | Data Science Tools              |
+# |---------------------------------|
+# | Jupyter Notebook                |
+# | Spyder                          |
+# | RStudio                         |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# #### This is a simple arithmetic expression to multiply then add integers
+
+# In[1]:
+
+
+(3 * 4) + 5
+
+
+# #### This will convert 200 minutes to hours by dividing by 60
+
+# In[2]:
+
+
+200 / 60
+
+
+# 
+# 
+
+# ## Author
+
+# Hemanth Narla Subramanyam
+
+# In[ ]:
+
+
+
+
